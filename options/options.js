d1c050023ea00eadb1c838044134359b46d93269
Crowdbotics <team@crowdbotics.com>
2022-07-27 19:08:33 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "malone_pharmacy_36448",
+  url: "https://malone_pharmacy_36448.botics.co",
+  api: "https://malone_pharmacy_36448.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
